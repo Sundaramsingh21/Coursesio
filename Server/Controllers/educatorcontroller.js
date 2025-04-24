c7157d339d9eae688b55011056b38dd133004018
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2025-04-24 21:16:13 +0530

    Update educatorcontroller.js

--- Server/Controllers/educatorcontroller.js
+++ Server/Controllers/educatorcontroller.js
@@ -1,7 +1,7 @@
 import { clerkClient } from '@clerk/express'
 import Course from '../Models/Course.js';
 import { v2 as cloudinary } from 'cloudinary'
-import { Purchase } from '../Models/purchase.js';
+import { Purchase } from '../Models/Purchase.js';
 import User from '../Models/User.js'
 
 //update role to educator
