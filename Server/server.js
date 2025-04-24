5f34521c3a6c6fbbc575cca36674b8b1f3736536
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2025-04-24 21:21:48 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -23,7 +23,7 @@ app.use(clerkMiddleware());
 
 //Routes
 // path : req is pass into json : controller
-app.post('/clerk', express.json(), clerkWebhooks)
+// app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user',express.json(), userRouter )
